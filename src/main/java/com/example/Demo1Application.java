cb8b5b93b2f6bf427ebaf69b02107a5e86d1acb5
Grigoris Vasileiou <128288612+GrigorisVasileiou@users.noreply.github.com>
2026-07-08 20:44:17 +0300

    Demo1Application.java

--- src/main/java/com/example/Demo1Application.java
+++ src/main/java/com/example/Demo1Application.java
@@ -3,34 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
-
-import java.awt.Desktop;
-import java.net.URI;
-
 @SpringBootApplication
 public class Demo1Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Demo1Application.class, args);
-    }
-
-    @Bean
-    public ApplicationListener<ApplicationReadyEvent> openAvastBrowserOnStart() {
-        return event -> {
-            new Thread(() -> {
-                try {
-                    String url = "http://localhost:8080/login";
-                    String avastPath = "C:\\Program Files\\AVAST Software\\Browser\\Application\\AvastBrowser.exe";
-
-                    Runtime.getRuntime().exec(new String[]{avastPath, url});
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }).start();
-        };
     }
 }
